4c6a498612fdfbb0fad64653d6ce0bb1864006ba
chandan-16 <chandangautam420.cg@gmail.com>
2025-07-16 11:01:11 +0530

    Practice done

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,170 +1,158 @@
 import React from 'react'
 
 const Practice = () => {
+  // type User = {
+  //   name: string;
+  //   detail: Detail;
+  //   address: Address;
+  // }
 
-    // const name: string = 'Chandan Gautam'
-    // console.log(name);
+  // const user1: User = {
+  //   name: 'chandan gautam',
+  //   detail: {
+  //     profession: 'MERN Stack Developer',
+  //     experience: 3
+  //   },
+  //   address: {
+  //     city: 'pune',
+  //     country: 'india'
+  //   }
+  // }
 
-    // const age: number = 26;
-    // console.log(age)
+  // console.log(user1)
 
-    // const fruitList: string[] = ['mango', 'apple', 'kiwi'];
-    // console.log(fruitList)
+  // type Detail = {
+  //   profession: string;
+  //   experience: number;
+  // }
 
-    // const positiveNumber: number[] = [1,2,3,4,5,6]
-    // console.log(positiveNumber)
+  // type Address = {
+  //   city: string;
+  //   country: string;
+  // }
 
-    // function sum(num1: number, num2: number): number {
-    //     return num1 + num2 ;
-    // }
-    // console.log(sum(10,20))
+//   type User = {
+//     name: string;
+//     detail: Detail;
+//     address: Address;
+//   }
 
-    // const substraction = (num1: number, num2: number): number => {
-    //     return num1 - num2;
-    // }
-    // console.log(substraction(10,6))
-
-    // let person: [string, number] = ['chandan gautam', 26]
-    // console.log(person)
-
-    // type User = {
-    //     name: string,
-    //     age: number,
-    //     email: string,
-    //     isMarried: boolean
-    // }
-    // const user1: User = {
-    //     name : 'chandan gautam',
-    //     age: 25,
-    //     email: 'chandangautam420.cg@gmail.com',
-    //     isMarried: false
-    // }
-    // console.log(user1)
-
-    // type User = {
-    //     name: string,
-    //     age: number,
-    //     email: string,
-    //     isMarried: boolean
-    // }
-
-    // const user1: User = {
-    //     name: 'Chandan Gautam',
-    //     age: 25,
-    //     email: 'chandan@gmail.com',
-    //     isMarried : true
-    // }
-    // console.log(user1);
-
-    // type User = {
-    //     name: string,
-    //     age: number,
-    //     email: string,
-    //     isMarried: boolean
-    // }
-
-    // const user1: User = {
-    //     name: 'chandan gautam',
-    //     age: 26,
-    //     email: 'chandangautam@gmail.com',
-    //     isMarried: true
-    // }
-
-    // console.log(user1)
-
-    // type User = {
-    //     name: string,
-    //     age: number,
-    //     email?: string
-    // }
-
-    // const user1: User = {
-    //     name: 'chandan gautam',
-    //     age: 24,
-    //     email: 'chandan@gmail.com'
-    // }
-    // console.log(user1)
-
-    // type User = {
-    //     isMarried?: boolean,
-    //     email?: string,
-    //     name: string,
-    //     age: number
-    // }
-
-    // const user1: User = {
-    //     isMarried: false,
-    //     name: 'chandan gautam',
-    //     age: 24 
-    // }
-    // console.log(user1)
-
-    // type Address = {
-    //     city: string;
-    //     country: string;
-    // }
-
-    // type User = {
-    //     name: string;
-    //     address: Address;
-    // }
-
-    // const user: User = {
-    //     name : 'Chandan Gautam',
-    //     address : {
-    //         city: 'Pune',
-    //         country: 'India'
-    //     }
-    // }
-    // console.log(user);
-
-    // type Address = {
-    //     city: string;
-    //     country: string;
-    // }
-
-    // type User = {
-    //     name: string;
-    //     address: Address
-    // }
-
-    // const user1: User = {
-    //     name: 'chandan gautam',
-    //     address: {
-    //         city: 'pune',
-    //         country: 'india'
-    //     }
-    // }
-    // console.log(user1);
-
-//     type Address = {
-//         city: string;
-//         country: string;
+//   const user1: User = {
+//     name: 'chandan gautam',
+//     detail : {
+//       profession: 'frontend developer',
+//       experience: 1,
+//     },
+//     address: {
+//       city: 'pune',
+//       country: 'india'
 //     }
+//   }
 
-//     type Detail = {
-//         profession: string;
-//         experience: number;
-//     }
+// console.log(user1)
 
-//     type User = {
-//         name: string;
-//         detail: Detail;
-//         address: Address;
-//     }
+// type Detail = {
+//   profession: string;
+//   experience: number
+// }
 
-//     const user: User = {
-//         name: 'chandan gautam',
-//         detail: {
-//             profession: 'frontend developer',
-//             experience : 2
-//         },
-//         address : {
-//             city: 'pune',
-//             country: 'india'
-//         }
-//     }
-// console.log(user)
+// type Address = {
+//   city: string;
+//   country: string;
+// }
+
+// type User = {
+//   name: string;
+//   email?: string;
+//   age: number;
+//   detail: Detail;
+//   address: Address;
+// };
+
+// type Detail = {
+//   profession: string;
+//   experience: number;
+// }
+
+// type Address = {
+//   city: string;
+//   country: string;
+// }
+
+// const user1: User = {
+//   name: 'chandan gautam',
+//   email: 'chandan@gmail.com',
+//   age: 25,
+//   address: {
+//     city: 'pune',
+//     country: 'india'
+//   },
+//   detail: {
+//     profession : 'Frontend Developer',
+//     experience : 2
+//   }
+// }
+// console.log(user1)
+
+// type User = {
+//   name: string;
+//   email?: string;
+//   age: number;
+//   address: Address;
+//   detail: Detail;
+// }
+
+// type Address = {
+//   city: string;
+//   country: string;
+// }
+
+// type Detail = {
+//   profession: string;
+//   experience: number;
+// }
+
+// const user1: User = {
+//   name: 'chandan gautam',
+//   email: 'chandangautam@gmail.com',
+//   age: 25,
+//   address : {
+//     city: 'pune',
+//     country: 'india'
+//   },
+//   detail: {
+//     profession: 'Frontend Developer',
+//     experience : 2
+//   }
+// };
+// console.log(user1)
+
+// const userList: string[] = ['chandan','roshan', 'suraj'];
+// console.log(userList)
+
+// const positiveNumbers: number[] = [1,2,3,4,5]
+// console.log(positiveNumbers)
+
+const data: number[] = [1,2,3,4]
+console.log(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
