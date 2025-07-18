cf754e9e46bd6869cd9d5232aaf404ac9f72004d
chandan-16 <chandangautam420.cg@gmail.com>
2025-07-18 15:19:59 +0530

    Practice typescript

---
+++ src/components/PracticeThree.tsx
@@ -0,0 +1,34 @@
+const PracticeThree = () => {
+
+    // enum Role  { ADMIN, AUTHOR, READ_USER_ONLY };
+
+    // const person = {
+    //     name : 'chandan gautam',
+    //     age: 25,
+    //     email: 'chandangautam@gmail.com',
+    //     skills: ['nextjs', 'typescript'],
+    //     role: Role.AUTHOR
+    // }
+
+    // if(person.role === Role.ADMIN){
+    //     console.log('ADMIN')
+    // }else if( person.role === Role.AUTHOR){
+    //     console.log('AUTHOR')
+    // }else if(person.role === Role.READ_USER_ONLY){
+    //     console.log('Read User Only')
+    // }
+
+
+
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PracticeThree
